f96fd8fe1c77fb50b453c5c6ee71a5bce0a7b642
Angelika Tyborska <atyborska93@gmail.com>
2015-10-09 11:41:54 +0200

    renamed a method

--- test/IdGeneratorTest.java
+++ test/IdGeneratorTest.java
@@ -1,11 +1,10 @@
 import static org.junit.Assert.assertEquals;
-
-import core.IdGenerator;
 import org.junit.Test;
+import core.IdGenerator;
 
 public class IdGeneratorTest {
     @Test
-    public void generatesSequentialIds() {
+    public void shouldGenerateSequentialIds() {
         int id1 = IdGenerator.getId();
         int id2 = IdGenerator.getId();
         assertEquals(id1 + 1, id2);
