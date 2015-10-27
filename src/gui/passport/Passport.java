47bcb53a20e7e411e28ea5f9e99f6aeed29dc503
Angelika Tyborska <atyborska93@gmail.com>
2015-10-27 11:59:27 +0100

    removed uncessary PassportController class

--- src/gui/passport/Passport.java
+++ src/gui/passport/Passport.java
@@ -1,19 +1,39 @@
 package gui.passport;
 
 import core.Passenger;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class Passport extends Group {
-  private Passenger passenger;
+  @FXML private Passenger passenger;
+  @FXML private Label firstName;
+  @FXML private Label lastName;
+  @FXML private Label age;
+  @FXML private Label PESEL;
+  @FXML private Label hometown;
 
   public Passport(Passenger passenger) {
     this.passenger = passenger;
 
-    PassportController passportController = new PassportController(passenger);
-    this.getChildren().add(passportController.getGridPane());
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(Passport.class.getResource("passport.fxml"));
+    loader.setController(this);
+    try {
+      getChildren().add(loader.load());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void initialize() {
+    firstName.setText(passenger.getFirstName());
+    lastName.setText(passenger.getLastName());
+    age.setText(passenger.getAge() + " years old");
+    PESEL.setText(passenger.getPESEL());
+    hometown.setText(passenger.getHometown());
   }
 }
